cdce493e89322253f89e11c851e278962d1abd21
jackmmd <ivanmondragonmanchay@gmail.com>
2025-04-14 09:33:37 -0500

    ✨ Update: formated data

--- controllers/incident.controller.js
+++ controllers/incident.controller.js
@@ -3,6 +3,12 @@ const incidentService = new IncidentService()
 class IncidentController {
   async create(req, res) {
     const { tipo_error, dni, asegurado, contratante, clinica, detalles } = req.body;
+    const fecha = new Date(); 
+    const day = String(fecha.getDate()).padStart(2, '0');
+    const month = String(fecha.getMonth() + 1).padStart(2, '0');
+    const year = fecha.getFullYear();
+
+    const date_formated = `${day}/${month}/${year}`;
     const text = `
     DNI: ${dni}
     Asegurado: ${asegurado}
@@ -11,7 +17,7 @@ class IncidentController {
     Tipo error: ${tipo_error}
     Detalles del error: ${detalles}`;
 
-    await incidentService.create([(new Date()).toLocaleDateString(),tipo_error, dni, asegurado, contratante, clinica, detalles])
+    await incidentService.create([date_formated,tipo_error, dni, asegurado, contratante, clinica, detalles],text)
     res.send('ok')
   }
 }
