8934dc5926660ff2ca81ffcb4c010ecac900d62a
jackmmd <ivanmondragonmanchay@gmail.com>
2025-04-14 06:21:49 -0500

    ✨ Update: config deploy

--- index.js
+++ index.js
@@ -3,6 +3,30 @@ const express = require('express')
 const bodyParser = require('body-parser');
 require('dotenv').config()
 const app = express();
+const whitelist = [
+    '*'
+  ];
+  
+  app.use((req, res, next) => {
+    const origin = req.get('referer');
+    const isWhitelisted = whitelist.find((w) => origin && origin.includes(w));
+    if (isWhitelisted) {
+      res.setHeader('Access-Control-Allow-Origin', '*');
+      res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+      res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,Authorization');
+      res.setHeader('Access-Control-Allow-Credentials', true);
+    }
+    // Pass to next layer of middleware
+    if (req.method === 'OPTIONS') res.sendStatus(200);
+    else next();
+  });
+  
+  const setContext = (req, res, next) => {
+    if (!req.context) req.context = {};
+    next();
+  };
+app.use(setContext);
+
 app.use(cors()); 
 const path = require('path');
 const { IncidentController } = require('./controllers/incident.controller');
