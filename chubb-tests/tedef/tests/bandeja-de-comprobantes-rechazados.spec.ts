5d9db102d603d49e55246041ea7b5defd20120c8
jackmmd <ivanmondragonmanchay@gmail.com>
2025-04-22 15:18:46 -0500

    🌱 Add: bandeja-de-comprobantes-rechazados.spec.ts

---
+++ chubb-tests/tedef/tests/bandeja-de-comprobantes-rechazados.spec.ts
@@ -0,0 +1,10 @@
+import { test } from '@playwright/test';
+test.use({ storageState:'auth.json' })
+test.describe('Tedef - Bandeja de comprobantes rechazados',()=>{
+    test('Bandeja de comprobantes rechazados',async({ page })=>{
+        await page.goto('http://localhost:4200/bandeja-de-comprobantes-rechazados');
+        await page.getByRole('button', { name: 'VER EVALUACIÓN' }).click();
+        await page.locator('#mat-dialog-title-0').getByRole('button').click();
+        await page.getByRole('button', { name: 'REVISAR' }).click()
+    })
+})
