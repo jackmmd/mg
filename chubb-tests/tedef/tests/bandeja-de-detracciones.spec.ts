a06df6c9f499d6d7b39757ff7b809dd2623318ef
jackmmd <ivanmondragonmanchay@gmail.com>
2025-04-24 12:46:33 -0500

    🌱 Add: new modules

---
+++ chubb-tests/tedef/tests/bandeja-de-detracciones.spec.ts
@@ -0,0 +1,21 @@
+import { test } from '@playwright/test'
+test.use({storageState:'auth.json'})
+test.describe('Tedef - Bandeja de detracciones',()=>{
+    test('Bandeja de detracción',async({page})=>{
+        await page.goto('http://localhost:4200/bandeja-de-detracciones');
+        await page.getByRole('button', { name: 'GENERAR REPORTE' }).click();
+        await page.getByRole('button', { name: 'Si' }).click();
+        await page.locator('mat-drawer-content a').filter({ hasText: 'Todos' }).click();
+        await page.locator('mat-drawer-content a').filter({ hasText: 'Pendiente' }).click();
+        await page.locator('mat-drawer-content a').filter({ hasText: 'Enviado' }).click();
+        await page.locator('mat-drawer-content a').filter({ hasText: 'Todos' }).click();
+        await page.getByRole('button', { name: 'DESCARTAR' }).click();
+        await page.getByRole('button', { name: 'Cancelar' }).click();
+        await page.getByRole('button', { name: 'REGISTRAR OP' }).click();
+        await page.locator('#mat-dialog-title-0').getByRole('button').click();
+        await page.getByRole('button', { name: 'REVISAR' }).click(); 
+    })
+})
+
+// Desde el 18 de abril
+// Job_apertura_siniestros
--- chubb-tests/tedef/tests/bandeja-de-lotes.spec.ts
+++ chubb-tests/tedef/tests/bandeja-de-lotes.spec.ts
@@ -1,5 +1,7 @@
 import { test } from '@playwright/test';
 
+
+
 test.use({ storageState:'auth.json' })
 
 test.describe('Tedef - Bandeja de lotes', () => {
@@ -8,29 +10,33 @@ test.describe('Tedef - Bandeja de lotes', () => {
     await page.goto('http://localhost:4200/bandeja-de-lotes');
     await page.getByRole('button', { name: 'CARGAR LOTE' }).click();
     await page.getByRole('button').filter({ hasText: /^$/ }).click();
-    await page.locator('mat-drawer-content a').filter({ hasText: 'Pendiente' }).click();
-    await page.locator('mat-drawer-content a').filter({ hasText: 'Aprobado' }).click();
-    await page.locator('mat-drawer-content a').filter({ hasText: 'Aprobación Parcial' }).click();
-    await page.locator('mat-drawer-content a').filter({ hasText: 'Rechazado' }).click();
-    await page.locator('mat-drawer-content a').filter({ hasText: 'Todos' }).click();
-    await page.getByRole('button', { name: 'VER CONST.' }).first().click();
-    await page.getByText('Envío de constancia').click();
-    await page.locator('#mat-dialog-title-1').getByRole('button').click();
-    await page.getByRole('button', { name: 'REVISAR' }).first().click();
-    await page.getByRole('button', { name: 'VER EVALUACIÓN' }).click();
-    await page.locator('#mat-dialog-title-2').getByRole('button').click();
-    await page.getByRole('button', { name: 'REVISAR' }).click();
+    
+    await Promise.all(
+      ['Pendiente','Aprobado','Aprobación Parcial','Rechazado','Todos'].map(async(item)=>{
+        await page.locator('mat-drawer-content a').filter({ hasText: item }).click();
+      })
+    )
+
+    await page.getByRole('button', { name: 'REVISAR' }).first()
+    await page.getByRole('row', { name: 'F35700319794 DNI 75124426 01-' }).getByRole('button').nth(1)
+    await page.getByRole('link', { name: 'Doc. Facturador' }).click();
     await page.getByRole('link', { name: 'General' }).click();
     await page.getByRole('link', { name: 'Detalles' }).click();
     await page.getByRole('link', { name: 'Farmacia' }).click();
     await page.getByRole('link', { name: 'Odontológico' }).click();
     await page.getByRole('link', { name: 'Archivos' }).click();
-    await page.getByRole('button', { name: 'Cargar Archivos' }).click();
-    await page.getByRole('button').filter({ hasText: /^$/ }).click();
-    await page.getByRole('button', { name: 'Ver evaluación' }).click();
-    await page.locator('#mat-dialog-title-4').getByRole('button').click();
-    await page.getByRole('button', { name: 'Re-evaluar' }).click();
-    await page.locator('#mat-dialog-title-5').click();
-    await page.getByRole('button').filter({ hasText: /^$/ }).click();
+    // await Promise.all(
+    //   ['General','Detalles','Farmacia','Odontológico','Archivos'].map(async(item)=>{
+    //     await page.getByRole('link', { name: item }).click();
+    //   })
+    // )
+    
+    // await page.getByRole('button', { name: 'Cargar Archivos' }).click();
+    // await page.getByRole('button').filter({ hasText: /^$/ }).click();
+    // await page.getByRole('button', { name: 'Ver evaluación' }).click();
+    // await page.locator('#mat-dialog-title-4').getByRole('button').click();
+    // await page.getByRole('button', { name: 'Re-evaluar' }).click();
+    // await page.locator('#mat-dialog-title-5').click();
+    // await page.getByRole('button').filter({ hasText: /^$/ }).click();
   });
 });
